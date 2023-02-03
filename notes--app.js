f4b68b87f17ee11693da8385438083796964ec3a
enes <eneskiliicc06@gmail.com>
2023-02-03 16:32:36 +0300

    filter search added and restored

--- notes--app.js
+++ notes--app.js
@@ -1,20 +1,48 @@
-const notes = [{},{
-    title: 'Trip',
-    body: 'Vacation'
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
 }, {
-    title: 'Love',
-    body: 'Story'
+    title: 'Habbits to work on',
+    body: 'Exercise, eating a bit better'
 }, {
-    title: 'Work',
-    body: 'Job'
+    title: 'Office modification',
+    body: 'Get a new seat'
 }]
 
-document.querySelector('#create-note').addEventListener('click', function(e){
+//filter
+const filters = {
+    searchText: ''
+}
+
+//?????????????????????????*
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {   //array filter method
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function (note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+//create new note
+document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = 'The button was clicked'
 })
 
-document.querySelector('#remove-all').addEventListener('click', function(e){
-    document.querySelectorAll('.note').forEach( function(note){
+//remove note
+document.querySelector('#remove-all').addEventListener('click', function (e) {
+    document.querySelectorAll('.note').forEach(function (note) {
         note.remove()
     })
+})
+
+//search filter
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
