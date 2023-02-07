f2ad9beadeed6b4457bd9da61c42f2be61d6f671
enes <eneskiliicc06@gmail.com>
2023-02-07 13:48:06 +0300

    refactoring

--- notes--app.js
+++ notes--app.js
@@ -1,36 +1,8 @@
-let notes = []
+const notes = getSavedNotes()
 
 //filter
 const filters = {
     searchText: ''
-}
-
-//check existing saved data
-const notesJSON = localStorage.getItem('notes')
-
-if (notesJSON !== null) {
-    notes = JSON.parse(notesJSON)
-}
-
-//?????????????????????????*
-const renderNotes = function (notes, filters) {
-    const filteredNotes = notes.filter(function (note) {   //array filter method
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-
-    document.querySelector('#notes').innerHTML = ''
-
-    filteredNotes.forEach(function (note) {
-        const noteEl = document.createElement('p')
-
-        if (note.title.length > 0) {
-            noteEl.textContent = note.title
-        } else {
-            noteEl.textContent = 'Unnamed note'
-        }
-
-        document.querySelector('#notes').appendChild(noteEl)
-    })
 }
 
 renderNotes(notes, filters)
@@ -41,7 +13,7 @@ document.querySelector('#create-note').addEventListener('click', function (e) {
         title: '',
         body: ''
     })
-    localStorage.setItem('notes', JSON.stringify(notes))
+    saveNotes(notes)
     renderNotes(notes, filters)
 })
 
